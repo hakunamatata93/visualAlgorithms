2f394d47b9227eb0c10db2a488590c3a0289b3b3
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-12-16 00:40:56 +0400

    generate array

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import styles from "./sorting-page.module.css";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { Direction } from "../../types/direction";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Column } from "../ui/column/column";
 
 export const SortingPage: React.FC = () => {
+  const [randomArray, setRandomArray] = useState<any>([])
+
+  const generateArray = () => {
+
+    let n 
+
+    const getRandomArbitrary = (min: any, max: any) => {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    n = getRandomArbitrary(3, 18)
+
+    const randomArr = Array(n).fill(null).map(() => Math.floor(Math.random() * 100))
+    console.log(randomArr)
+
+    setRandomArray(randomArr)
+
+
+
+  }
   return (
     <SolutionLayout title="Сортировка массива">
-      <main className={styles.main}>
+      <section className={styles.main}>
         <div className={styles.radiobutton}>
           <RadioInput 
             label = "Выбор" 
@@ -28,10 +49,26 @@ export const SortingPage: React.FC = () => {
             sorting={Direction.Descending}
           />
         </div>
-        <Button 
+        <Button onClick={generateArray}
           text='Новый массив'
         />
-      </main>
+      </section>
+      <section className={styles.columns}>
+      {
+        randomArray.map((item: any, index: any) => {
+          return (
+            <Column
+              index={item}
+              key={index}
+            />
+          )
+        })
+      }
+
+
+
+
+      </section>
     </SolutionLayout>
   );
 };
