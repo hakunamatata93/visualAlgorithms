1b826682490d4a695c3bdf37d2466b9111af8204
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-12-15 18:11:50 +0400

    string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import styles from "./string.module.css";
@@ -6,30 +6,80 @@ import { Circle } from "../ui/circle/circle";
 import { ElementStates } from "../../types/element-states";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+interface StringProps {
+  symbol: string,
+  state?: ElementStates
+}
+
 export const StringComponent: React.FC = () => {
+
+  const [inputValue, setInputValue] = useState('')
+
+  const [showValue, setShowValue] = useState<StringProps[]>([])
+
+  const onButtonClick = useCallback(() => {
+
+    const inputs = inputValue.split('').map(item=>
+      {
+        return {
+          symbol: item,
+          state: ElementStates.Default
+        }
+      }
+    )
+
+    setShowValue(inputs)
+    let i = 0
+
+    let x = setInterval(() => {
+
+      let arr = [...inputs]
+
+      arr[i] = {
+        symbol: inputs[inputs.length - 1 - i].symbol,
+        state: ElementStates.Changing
+      }
+      arr[arr.length - 1 - i] = {
+        symbol: inputs[i].symbol,
+        state: ElementStates.Changing
+      }
+
+      i = i + 1
+      if (i > arr.length/2) {
+        clearInterval(x)
+      }
+
+      setShowValue(arr)
+
+    }, 1000)
+
+  }, [inputValue])
+
   return (
     <SolutionLayout title="Строка">
        <section className={styles.main}>
         <Input 
           maxLength={11}
           isLimitText
+          extraClass = ""
+          onChange = {(e) => setInputValue(e.currentTarget.value)}
         />
-        <Button 
+        <Button onClick={onButtonClick}
           text='Развернуть'/>
       </section>
 
       <section className={styles.circles}>
-        <Circle 
-          letter='1'
-          state = {ElementStates.Changing}
-        />
-        <Circle 
-          letter='3'
-        />
-        <Circle
-          letter='5'
-          state = {ElementStates.Modified}
-        />
+      {
+        showValue.map((item, index) => {
+          return (
+            <Circle 
+              letter={item.symbol} 
+              state={item.state} 
+              key={index}
+            />
+          )
+        })
+      }
       </section>
     </SolutionLayout>
   );
