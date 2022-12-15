a929848dd9bc5638bebedf06b3ab96b061ce4e78
hakunamatata93 <ivaschenkodavid@yandex.ru>
2022-12-15 17:48:54 +0400

    fibonacci layout

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from "./fibonacci-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const FibonacciPage: React.FC = () => {
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+          <main className={styles.main}>
+        <Input 
+
+          isLimitText
+          max={19}
+          type=''
+
+
+        />
+        <Button 
+          text='Рассчитать'/>
+      </main>
     </SolutionLayout>
   );
 };
